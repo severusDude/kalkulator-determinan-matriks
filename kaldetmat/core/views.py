3a867a759914327c4089e098dea73a2f205af51a
severusDude <fajarsaladin05@gmail.com>
2024-05-01 21:27:38 +0700

    add simple views for index and calculate

--- kaldetmat/core/views.py
+++ kaldetmat/core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from .functions import *
 
 # Create your views here.
+
+
+@require_http_methods(['GET', ])
+def index(request):
+
+    return render(request, 'core/index.html')
+
+
+@require_http_methods(['POST', ])
+def calculate(request):
+
+    pass
