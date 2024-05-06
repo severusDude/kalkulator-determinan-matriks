a3c12587585a3428fa042415e29ac103390dfa66
severusDude <fajarsaladin05@gmail.com>
2024-05-06 11:57:24 +0700

    add views for calculation

--- kaldetmat/core/views.py
+++ kaldetmat/core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseBadRequest
 from django.views.decorators.http import require_http_methods
+from . import functions
 
 # Create your views here.
 
@@ -10,7 +12,21 @@ def index(request):
     return render(request, 'core/index.html')
 
 
-@require_http_methods(['POST', ])
-def calculate(request):
+@require_http_methods(['GET', 'POST', ])
+def calculate(request, result=[]):
 
-    return render(request, 'core/calculate.html')
+    if request.method == "POST":
+
+        data = request.POST
+        result = functions.find_determinant(data)
+
+        if len(result) == 0:
+            return HttpResponseBadRequest()
+
+        return redirect("calculate", result=result)
+
+    elif request.method == "GET":
+
+        return render(request, 'core/calculate.html', {
+            'result': result
+        })
