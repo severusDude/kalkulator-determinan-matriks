e635a87467b7e37014c4082e2d7f6cb839b6f419
iqbal-mn694 <garenahiber134@gmail.com>
2024-05-08 10:46:33 +0700

    delete new endpoint

--- kaldetmat/core/urls.py
+++ kaldetmat/core/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = "core"
-
 urlpatterns = [
     path('', views.index, name="index"),
     path('calculate/', views.calculate, name="calculate"),
-    path('calculate/', views.calculateForm, name="calculate"),
 ]
